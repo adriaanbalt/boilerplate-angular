daeb4ba7d91f0bba5491efcca498200b862aeede
adriaanbalt <adriaan@balt.us>
2015-05-31 18:57:16 -0400

    using require via browserify to include the widget module inside the Angular app

--- app/assets/js/app.js
+++ app/assets/js/app.js
@@ -1,43 +1,9 @@
-"use strict";
+(function () {
+    "use strict";
 
-angular.module('boilerplate')
+    require('./vendor/angular');
 
-/**
- * @ngdoc service
- * @name boilerplate.service
- * @description
- * 
- */
-.factory('Service', ['$http', function($http) {
+    var App = angular.module('boilerplate', []);
+    require('./widget')(App);
 
-    var _service = {};
-
-    return _service;
-        
-}])
-
-/**
- * @ngdoc directive
- * @name boilerplate.directive
- * @description
- * 
- */
-.directive('screen', [function() {
-    return {
-        restrict: 'ECA',
-        replace: false,
-        controller: "Controller",
-        link: function($scope, elem, attrs) {
-        }
-    }
-}])
-
-/**
- * @ngdoc controller
- * @name boilerplate.controller
- * @description
- * 
- */
-.controller('Controller', ['$scope', '$rootScope', 'Service', function($scope, $rootScope, Service) {
-        
-}]);
+}());
