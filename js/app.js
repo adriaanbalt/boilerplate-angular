ce42a751b718460d5fbedfdf25c36958d8bcfa0b
adriaanbalt <adriaan@balt.us>
2015-05-31 13:38:59 -0400

    foundation for the angular app

---
+++ js/app.js
@@ -0,0 +1,28 @@
+"use strict";
+
+angular.module('boilerplate')
+/**
+ * @ngdoc service
+ * @name boilerplate.service
+ * @description
+ * 
+ */
+.factory('Service', ['$http', function($http) {
+
+    var _service = {};
+
+    return _service;
+        
+}])
+.directive('screen', [function() {
+    return {
+        restrict: 'ECA',
+        replace: false,
+        controller: "Controller",
+        link: function($scope, elem, attrs) {
+        }
+    }
+}])
+.controller('Controller', ['$scope', '$rootScope', function($scope, $rootScope) {
+        
+}]);
